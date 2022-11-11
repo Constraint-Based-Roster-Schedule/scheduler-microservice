35bc176f835c0d5722611ee423ecfcc246dc09c4
centurion6x <gamunu99sb@gmail.com>
2022-11-11 16:10:50 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config.from_pyfile("config.py") 
+
+@app.route('/')
+def index():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
